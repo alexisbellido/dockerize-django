ac387c40473f02363ad61ce3c1dbb8c8388f9f13
Alexis Bellido <alexis@ventanazul.com>
2016-11-06 20:12:41 -0500

    check if editable python packages already installed

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 set -e
 
-source /root/.venv/zinibu/bin/activate
-pip install --requirement /tmp/editable-requirements.txt
+source /root/.venv/project/bin/activate
+
+# Install editable applications from mounted volume if required
+python -c 'import znbcache' 
+if [ $? -eq 1 ]; then
+	pip install --requirement /tmp/editable-requirements.txt
+fi
+
+# TODO accept arguments from from docker run using CMD in Dockerfile
+# TODO use it to indicate settings file and other options
 
 if [ "$1" = 'runserver' ]; then
 	exec gosu root django-admin runserver --settings=zinibu.settings.locals3 --pythonpath=$(pwd) 0.0.0.0:8000
