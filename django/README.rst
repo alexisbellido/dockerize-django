2e95420992008bce353969c718e3c9fa4f0307a7
Alexis Bellido <alexis@ventanazul.com>
2016-11-08 00:44:12 -0500

    my entry point is running django development server and gunicorn now

--- django/README.rst
+++ django/README.rst
@@ -12,6 +12,15 @@ Build the image from this directory:
 
   ``docker build -t alexisbellido/python:v5 .``
 
-Run a container from the image:
+Run the container passing parameters.
 
-  ``docker run -itd --network=zinibu -v /home/alexis/mydocker/project:/root/project -v /home/alexis/mydocker/djapps:/root/djapps --hostname=app1 -p 192.168.1.202:33001:8000 --name=app1 alexisbellido/python:v5``
+For Django development server:
+  ``docker run -itd --network=zinibu -v /home/alexis/mydocker/zinibu:/root/zinibu -v /home/alexis/mydocker/djapps:/root/djapps --env PROJECT_NAME=zinibu --env PORT=8000 --env SETTINGS_MODULE=locals3 -P --hostname=app8 --name=app8 alexisbellido/python:v9 development``
+
+For Django via gunicorn (specifying how to map the port on the host):
+  ``docker run -itd --network=zinibu -v /home/alexis/mydocker/zinibu:/root/zinibu -v /home/alexis/mydocker/djapps:/root/djapps --env PROJECT_NAME=zinibu --env PORT=8000 --env SETTINGS_MODULE=locals3 -p 192.168.1.202:33001:8000 --hostname=app9 --name=app9 alexisbellido/python:v9 production``
+
+Note the environment variables:
+SETTINGS_MODULE, used for DJANGO_SETTINGS_MODULE
+PROJECT_NAME, the name of your project
+PORT
--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
-source /root/.venv/project/bin/activate
+# Activate virtual environment
+source /root/.venv/django/bin/activate
+
+# Variables from Dockerfile: PROJECT_NAME, PORT, SETTINGS_MODULE
+export PROJECTDIR=/root/$PROJECT_NAME
+export DJANGO_SETTINGS_MODULE=$PROJECT_NAME.settings.$SETTINGS_MODULE
+
+cd $PROJECTDIR
 
 # Install editable applications from mounted volume if required
 python -c 'import znbcache' 
@@ -8,11 +15,46 @@ if [ $? -eq 1 ]; then
 	pip install --requirement /tmp/editable-requirements.txt
 fi
 
-# TODO accept arguments from from docker run using CMD in Dockerfile
-# TODO use it to indicate settings file and other options
+if [ "$1" == "development" ]; then
+	export PROJECT_RUNNING_DEV=true
+	exec gosu root django-admin runserver --pythonpath=$(pwd) 0.0.0.0:$PORT
+elif [ "$1" == "production" ]; then
+	export PROJECT_RUNNING_DEV=false
+	export USER=root
+    	export GROUP=root
+	export NUM_WORKERS=3
+	export BIND_HOST=`cat /etc/hostname`
+	export BIND_PORT=$PORT
+	export LOGFILE=/var/log/$PROJECT_NAME.log
 
-if [ "$1" = 'runserver' ]; then
-	exec gosu root django-admin runserver --settings=zinibu.settings.locals3 --pythonpath=$(pwd) 0.0.0.0:8000
+	if [ "$2" == "--log-level=debug" ]; then
+		export LOGLEVEL=debug
+	elif [ "$2" == "--log-level=critical" ]; then
+		export LOGLEVEL=critical
+	else
+		export LOGLEVEL=info
+	fi
+
+	exec gosu root gunicorn --workers=$NUM_WORKERS --user=$USER --group=$GROUP --bind $BIND_HOST:$BIND_PORT --log-level=$LOGLEVEL --log-file=$LOGFILE 2>>$LOGFILE $PROJECT_NAME.wsgi:application
+
+elif [ "$1" == "update_index" ]; then
+	django-admin.py update_index --age=$2 --pythonpath=$(pwd)
+
+elif [ "$1" == "shell" ]; then
+	django-admin.py shell --pythonpath=$(pwd)
+
+elif [ "$1" == "setenv" ]; then
+	echo "==================================="
+	echo "Done! The environment variables have been set to run the $PROJECT_NAME project."
+	echo "==================================="
+
+elif [ "$1" == "collectstatic" ]; then
+	echo "==================================="
+	echo "Django collect static files"
+	echo "==================================="
+	django-admin.py collectstatic --pythonpath=$(pwd) --noinput
+
+else
+	exec "$@"
+
 fi
-
-exec "$@"
