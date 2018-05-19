f84161b796a9a6aadafcbef62c561b606f7fe997
Alexis Bellido <alexis@ventanazul.com>
2018-05-19 19:42:49 -0400

    gumby

--- basic-python/a.py
+++ basic-python/a.py
@@ -2,3 +2,4 @@ import b
 
 print(b.name)
 print(b.name_from_c)
+b.spam('gumby')
--- basic-python/b.py
+++ basic-python/b.py
@@ -3,3 +3,6 @@ from c import name as name_from_c
 
 name = 'module b'
 # name_from_c = c.name
+
+def spam(text):
+    print(text, 'spam')
